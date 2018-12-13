b5fb0d17f274814ce4806153707ff38b473cdce4
Huffyphenix <949985347@qq.com>
2018-12-13 09:48:04 +0800

    validation of clusters by survival analysis

--- R-pac-learning/CancerSubtypes_learning.R
+++ R-pac-learning/CancerSubtypes_learning.R
@@ -27,7 +27,27 @@ res3=data.imputation(mRNA,fun="microarray")
 result1=data.normalization(mRNA,type="feature_Median",log2=FALSE)
 result2=data.normalization(mRNA,type="feature_zscore",log2=FALSE)
 
+# # 2.2 Feature selection
+# 2.2.1 Feature selection based on the most variance.
+###The top 1000 most variance features will be selected.
+data1=FSbyVar(mRNA, cut.type="topk",value=1000)
 
+###The features with (variance>0.5) are selected.
+data2=FSbyVar(mRNA, cut.type="cutoff",value=0.5)
+
+# 2.2.2 Feature selection based on the most variant Median Absolute Deviation (MAD).
+data1=FSbyMAD(mRNA, cut.type="topk",value=1000)
+data2=FSbyMAD(mRNA, cut.type="cutoff",value=0.5)
+
+# 2.2.3 Feature dimension reduction and extraction based on Principal Component Analysis.
+mRNA1=data.imputation(mRNA,fun="microarray")
+data1=FSbyPCA(mRNA1, PC_percent=0.9,scale = TRUE)
+
+# 2.2.4 Feature selection based on Cox regression model.
+data(GeneExp)
+data(time)
+data(status)
+data1=FSbyCox(GeneExp,time,status,cutoff=0.05)
 # clustering methods--------------------------------------------------------------
 
 ## 1. Consensus Clustering for cancer subtype identification ------------------
@@ -128,3 +148,75 @@ plot(sil)
 # All the samples have the negative silhouette width.
 sil1=silhouette(result$group, result$distanceMatrix)
 plot(sil1)  ##wrong result
+
+
+## 2. survival analysis -----------------------------------------------------
+### Survival analysis is used to judge the different survival patterns between subtypes.
+
+data(GeneExp)
+data(miRNAExp)
+data(time)
+data(status)
+data1=FSbyCox(GeneExp,time,status,cutoff=0.05)
+data2=FSbyCox(miRNAExp,time,status,cutoff=0.05)
+GBM=list(GeneExp=data1,miRNAExp=data2)
+
+#### 2.1.ExecuteSNF
+result1=ExecuteSNF(GBM, clusterNum=3, K=20, alpha=0.5, t=20,plot = FALSE)
+group1=result1$group
+distanceMatrix1=result1$distanceMatrix
+p_value=survAnalysis(mainTitle="GBM1",time,status,group1,
+                     distanceMatrix1,similarity=TRUE)
+### This is a combination figure with three parts: Survival curves, heatmap of the sample similarity matrix and Silhouette width plots for the identified cancer subtypes.
+
+#### 2.2.ExecuteSNF.CC
+result2=ExecuteSNF.CC(GBM, clusterNum=3, K=20, alpha=0.5, t=20,
+                      maxK = 5, pItem = 0.8,reps=500, 
+                      title = "GBM2", plot = "png",
+                      finalLinkage ="average")
+group2=result2$group
+distanceMatrix2=result2$distanceMatrix
+p_value=survAnalysis(mainTitle="GBM2",time,status,group2,
+                     distanceMatrix2,similarity=TRUE)
+
+
+#### 3 Statistical significance of clustering---------
+## Statistical significance of clustering is a pure statistical approach to test the significance difference data distribution between subtypes. Different expression is to test the expression difference between each subtypes and a reference group (always a set of normal samples).
+
+data(GeneExp)
+data(miRNAExp)
+data(time)
+data(status)
+GBM=list(GeneExp=GeneExp,miRNAExp=miRNAExp)
+result=ExecuteSNF(GBM, clusterNum=3, K=20, alpha=0.5, t=20,plot = FALSE)
+group=result$group
+sigclust=sigclustTest(miRNAExp,group, nsim=1000, nrep=1, icovest=1) #A statistical method for testing the significance of clustering results.
+sigclust 
+
+
+### 4. Differential expression analysis -------------------
+###Differential expression analysis is to test the expression difference between each subtypes and a reference group (always a set of normal samples). Here we apply limma package to conduct the different expression analysis between each subtypes and normal samples.
+
+library("RTCGA.mRNA")
+#require(TCGAbiolinks)
+rm(list = ls())
+data(BRCA.mRNA)
+mRNA=t(as.matrix(BRCA.mRNA[,-1]))
+colnames(mRNA)=BRCA.mRNA[,1]
+mRNA1=data.imputation(mRNA,fun="microarray")
+mRNA1=FSbyMAD(mRNA1, cut.type="topk",value=5000)
+###Split the normal and tumor samples
+index=which(as.numeric(substr(colnames(mRNA1),14,15))>9)
+mRNA_normal=mRNA1[,index]
+mRNA_tumor=mRNA1[,-index]
+
+### Remove the duplicate samples
+index1=which(as.numeric(substr(colnames(mRNA_tumor),14,15))>1)
+mRNA_tumor=mRNA_tumor[,-index1]
+
+##### Identify cancer subtypes
+result=ExecuteCC(clusterNum=5,d=mRNA_tumor,maxK=5,clusterAlg="hc",distance="pearson",title="BRCA")
+group=result$group
+res=DiffExp.limma(Tumor_Data=mRNA_tumor,Normal_Data=mRNA_normal,group=group,topk=NULL,RNAseq=FALSE)
+## Differently expression genes in subtype 1
+head(res[[1]])
